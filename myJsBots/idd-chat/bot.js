13fef1773ac09410e8b36acd3a2ba16e2c33bc7d
ChanYvonne <ychan1352@gmail.com>
2020-06-28 00:44:27 -0400

    changing it so that it sends text messages

--- myJsBots/idd-chat/bot.js
+++ myJsBots/idd-chat/bot.js
@@ -7,9 +7,29 @@ class EchoBot extends ActivityHandler {
     constructor() {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        // this.onMessage(async (context, next) => {
+        //     const replyText = `Echo: ${ context.activity.text }`;
+        //     await context.sendActivity(MessageFactory.text(replyText, replyText));
+        //     // By calling next() you ensure that the next BotHandler is run.
+        //     await next();
+        // });
+
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            const text = context.activity.text;
+
+            // Create an array with the valid color options.
+            const validColors = ['Red', 'Blue', 'Yellow'];
+
+            // If the `text` is in the Array, a valid color was selected and send agreement.
+            if (validColors.includes(text)) {
+                await context.sendActivity(`I agree, ${ text } is the best color.`);
+            } else {
+                await context.sendActivity('Please select a color.');
+            }
+
+            // After the bot has responded send the suggested actions.
+            await this.sendSuggestedActions(context);
+
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
@@ -25,6 +45,10 @@ class EchoBot extends ActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+    }
+    async sendSuggestedActions(turnContext) {
+        var reply = MessageFactory.suggestedActions(['Red', 'Yellow', 'Blue'], 'What is the best color?');
+        await turnContext.sendActivity(reply);
     }
 }
 
